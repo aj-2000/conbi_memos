ccda4cc81274f3a52e770a2fce421365fceb982d
Ajay Sharma <hi@ajaysharma.dev>
2022-11-26 21:20:27 +0530

    all features completed

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,17 @@ import { FiLogOut } from "react-icons/fi";
 import AuthForm from "./components/AuthForm";
 import supabase from "./utils/supabase.js";
 import CodeEditor from "@uiw/react-textarea-code-editor";
-import toast, { Toaster } from "react-hot-toast";
 import { v4 as uuidv4 } from "uuid";
 import LoadingSpinner from "./components/LoadingSpinner";
+import { ToastContainer, toast } from "react-toastify";
+import { IoMdAdd } from "react-icons/io";
+import { CiSaveUp1 } from "react-icons/ci";
+import { AiOutlineDelete } from "react-icons/ai";
+import { FcCheckmark } from "react-icons/fc";
+import "react-toastify/dist/ReactToastify.css";
 
+import moment from "moment";
+import { languages } from "./data/languages";
 type Todo = {
   task: string;
   isCompleted: boolean;
@@ -17,14 +24,17 @@ type MemoBlock = {
   id: string;
   text: string;
   code: string;
+  codeType: string;
   todos: Todo[];
+  created_at: string;
 };
-
+const notify = (message: string) => toast(message);
 function App() {
   const [user, setUser] = useState<any>(null);
-  const [task, setTask] = useState<string>();
-  const [text, setText] = useState<string>();
-  const [code, setCode] = useState<string>();
+  const [task, setTask] = useState<string>("");
+  const [text, setText] = useState<string>("");
+  const [code, setCode] = useState<string>("");
+  const [codeType, setCodeType] = useState<string>("js");
   const [todos, setTodos] = useState<Todo[]>([]);
   const [updated, setUpdated] = useState<string>();
   const [memos, setMemos] = useState<MemoBlock[]>([]);
@@ -35,9 +45,11 @@ function App() {
         .getSession()
         .then(({ data, error }: any) => {
           setUser(data.session.user);
+          notify("Signed in successfully.");
         })
         .catch((err: any) => {
           console.log(err);
+          notify("An error occured while signing in.");
         });
     }
   }, []);
@@ -71,37 +83,44 @@ function App() {
     await supabase.auth.signOut().then(({ error }: any) => {
       if (error) {
         console.log(error);
+        notify("An error occured while logging out.");
       } else {
         setUser(null);
         setIsLoading(false);
+        notify("Logged out successfully.");
       }
     });
   };
 
   const saveMemo = () => {
     setIsLoading(false);
-    supabase
-      .from("memos")
-      .insert({
-        text: text,
-        code: code,
-        codeType: "js",
-        userId: user?.id,
-        todos: todos,
-      })
-      .then(({ data, error }) => {
-        if (!error) {
-          setTask("");
-          setText("");
-          setCode("");
-          setTodos([]);
-          toast("Memo successfully saved!");
-          setUpdated(Date.now().toString());
-        } else {
-          toast("Failed to save memo.");
-        }
-        setIsLoading(false);
-      });
+    if (todos.length > 0 || text !== "" || code !== "") {
+      supabase
+        .from("memos")
+        .insert({
+          text: text,
+          code: code,
+          userId: user?.id,
+          todos: todos,
+          codeType: codeType,
+        })
+        .then(({ data, error }) => {
+          if (!error) {
+            setTask("");
+            setText("");
+            setCode("");
+            setTodos([]);
+            setCodeType("js");
+            notify("Memo successfully saved.");
+            setUpdated(Date.now().toString());
+          } else {
+            notify("Failed to save memo.");
+          }
+          setIsLoading(false);
+        });
+    } else {
+      notify("Empty memo can not be saved.");
+    }
   };
   const deleteMemo = async (id: string) => {
     setIsLoading(false);
@@ -109,15 +128,17 @@ function App() {
     if (error) {
       console.log(error);
       setIsLoading(false);
+      notify("An error occurred while deleting the memo.");
     } else {
       setUpdated(Date.now().toString());
       setIsLoading(false);
+      notify("Memo deleted successfully.");
     }
   };
   return (
     <div className="App">
       <div>
-        <Toaster />
+        <ToastContainer />
       </div>
       {user?.role ? (
         <div className="w-screen h-screen overflow-auto bg-gray-100 flex flex-col items-center">
@@ -148,55 +169,87 @@ function App() {
           {isLoading ? (
             <LoadingSpinner />
           ) : (
-            <div className="flex flex-col w-[800px]">
-              <span>Memos</span>
-              <div className="bg-white p-4 rounded flex flex-col gap-4 border-1 border-gray-200 mx-12 my-6 shadow">
+            <div className="flex flex-col gap-4 my-10 w-full px-2 md:px-auto md:w-[800px]">
+              <span className="text-center text-5xl font-mono font-extralight uppercase">
+                Conbi Memos
+              </span>
+              <div className="bg-white p-4 rounded flex flex-col gap-4 border-1 border-gray-200 my-6 shadow">
                 <textarea
                   onChange={(e) => setText(e.target.value)}
-                  className="p-2"
+                  className="p-2 bg-gradient-to-t rounded shadow-sm border-2 border-gray-200"
                   rows={4}
                   value={text}
                   placeholder="Any Thoughts..."
                 />
                 <div className="flex flex-col gap-2">
-                  <span>Tasks</span>
-                  <div className="flex justify-between">
+                  <span className="uppercase text-sky-500 font-bold">
+                    #Tasks
+                  </span>
+                  <div className="flex justify-between gap-2">
                     <input
                       onChange={(e) => setTask(e.target.value)}
                       type="text"
                       value={task}
-                      className="border w-[500px]"
+                      className="p-2 bg-gradient-to-t rounded border shadow-sm border-gray-200 w-full"
                     />
                     <button
+                      className="p-2 flex gap-2 bg-sky-500 text-white rounded shadow-sm"
                       onClick={() => {
-                        setTodos((prevTodos) => [
-                          ...prevTodos,
-                          {
-                            task: task || "",
-                            isCompleted: false,
-                            id: uuidv4(),
-                          },
-                        ]);
-                        setTask("");
+                        if (task) {
+                          setTodos((prevTodos) => [
+                            ...prevTodos,
+                            {
+                              task: task,
+                              isCompleted: false,
+                              id: uuidv4(),
+                            },
+                          ]);
+                          setTask("");
+                        }
                       }}
                     >
-                      Add
+                      <IoMdAdd size={25} />
+                      <span className="uppercase">Add</span>
                     </button>
                   </div>
-                  {todos.map((todo) => {
-                    return (
-                      <span key={todo.id} className="text-black">
-                        {todo.task}
-                      </span>
-                    );
-                  })}
+                  <div className="flex flex-col gap-2">
+                    {todos.map((todo, i) => {
+                      return (
+                        <span
+                          key={todo.id}
+                          className="text-white bg-sky-500 rounded-md p-2 shadow-sm"
+                        >
+                          {`#${i + 1} ${todo.task}`}
+                        </span>
+                      );
+                    })}
+                  </div>
                 </div>
                 <div className="flex flex-col gap-2">
-                  <span>Enter Some JS Code</span>
+                  <div className="flex gap-2">
+                    <span className="uppercase text-sky-500 font-bold">
+                      #Code
+                    </span>
+
+                    <select
+                      name="language"
+                      id="language"
+                      onChange={(e) => {
+                        setCodeType(e.target.value);
+                        console.log(e.target.value);
+                      }}
+                      defaultValue="js"
+                    >
+                      {languages.map((lang) => (
+                        <option value={lang.extension}>{lang.language}</option>
+                      ))}
+                    </select>
+                  </div>
+
                   <CodeEditor
                     value={code}
-                    language="js"
-                    placeholder="Please enter JS code."
+                    language={codeType}
+                    placeholder="Please enter some code."
                     onChange={(evn) => setCode(evn.target.value)}
                     padding={15}
                     style={{
@@ -208,91 +261,147 @@ function App() {
                   />
                 </div>
                 <div>
-                  <button onClick={saveMemo}>Save</button>
+                  <button
+                    className="p-2 flex gap-2 bg-green-500 text-white rounded"
+                    onClick={saveMemo}
+                  >
+                    <CiSaveUp1 size={25} />
+                    <span className="uppercase">Save</span>
+                  </button>
                 </div>
               </div>
-              <>
+              <div className="flex flex-col gap-4">
                 {memos.map((memo, memoIndex) => {
                   return (
                     <>
                       <div
                         key={memo.id}
-                        className="bg-white p-4 rounded flex flex-col"
+                        className="bg-white p-4 rounded flex flex-col gap-4 hover:border-2 hover:border-gray-200 hover:shadow"
                       >
-                        <p key={memo.id + "p"}>{memo.text}</p>
-                        <div key={memo.id + "div"} className="flex flex-col">
-                          <span key={memo.id + "span"}>Tasks</span>
-                          {memo.todos.map((todo, todoIndex) => {
-                            return (
-                              <>
-                                <div
-                                  className="flex justify-between"
-                                  key={todo.id}
-                                >
+                        <div className="text-sm text-gray-400 ">
+                          {moment(memo.created_at).utcOffset(330).format("MM/DD/YYYY HH:mm:ss")}
+                        </div>
+
+                        {memo.text !== "" ? <p>{memo.text}</p> : null}
+                        {memo.todos.length > 0 ? (
+                          <div className="flex flex-col gap-2">
+                            <span
+                              className="uppercase text-sky-500 font-bold"
+                              key={memo.id + "span"}
+                            >
+                              #Tasks
+                            </span>
+
+                            {memo.todos.map((todo, todoIndex) => {
+                              return (
+                                <>
                                   <div
-                                    key={todo.id + "div"}
-                                    onClick={() => {
-                                      setIsLoading(true);
-                                      setMemos((prevMemos) => {
-                                        prevMemos[memoIndex].todos[
-                                          todoIndex
-                                        ].isCompleted = !todo.isCompleted;
+                                    className="flex gap-2 items-center"
+                                    key={todo.id}
+                                  >
+                                    <div
+                                      key={todo.id + "div"}
+                                      onClick={() => {
+                                        setMemos((prevMemos) => {
+                                          prevMemos[memoIndex].todos[
+                                            todoIndex
+                                          ].isCompleted = !todo.isCompleted;
 
-                                        supabase
-                                          .from("memos")
-                                          .update({
-                                            todos: prevMemos[memoIndex].todos,
-                                          })
-                                          .match({ id: memo.id })
-                                          .then(({ data, error }) => {
-                                            setIsLoading(false);
-                                            if (error) console.log(error);
-                                            else fetchMemos();
-                                          });
-                                        return prevMemos;
-                                      });
-                                    }}
-                                    className={`w-4 h-4 rounded-full border border-black ${
-                                      todo?.isCompleted ? "bg-black" : null
-                                    }`}
-                                  />
+                                          supabase
+                                            .from("memos")
+                                            .update({
+                                              todos: prevMemos[memoIndex].todos,
+                                            })
+                                            .match({ id: memo.id })
+                                            .then(({ data, error }) => {
+                                              if (error) {
+                                                toast(
+                                                  "An error occured while updating response."
+                                                );
+                                                console.log(error);
+                                              } else {
+                                                supabase
+                                                  .from("memos")
+                                                  .select()
+                                                  .order("created_at", {
+                                                    ascending: false,
+                                                  })
+                                                  .eq("userId", user?.id)
+                                                  .then(({ data, error }) => {
+                                                    const memosData: MemoBlock[] =
+                                                      [];
+                                                    data?.map((memo) => {
+                                                      memosData.push(memo);
+                                                    });
+                                                    setMemos(memosData);
+                                                    toast(
+                                                      "Reponse updated successfully."
+                                                    );
+                                                  });
+                                              }
+                                            });
+                                          return prevMemos;
+                                        });
+                                      }}
+                                      className="flex justify-center items-center p-[0.5px] w-6 h-6 rounded-full border border-black"
+                                    >
+                                      {" "}
+                                      {todo?.isCompleted ? (
+                                        <FcCheckmark size={20} />
+                                      ) : null}{" "}
+                                    </div>
 
-                                  <p key={todo.id + "p"} className="">
-                                    {todo?.task}
-                                  </p>
-                                </div>
-                              </>
-                            );
-                          })}
-                        </div>
+                                    <p
+                                      key={todo.id + "p"}
+                                      className={`${
+                                        todo?.isCompleted
+                                          ? "line-through"
+                                          : null
+                                      }`}
+                                    >
+                                      {todo?.task}
+                                    </p>
+                                  </div>
+                                </>
+                              );
+                            })}
+                          </div>
+                        ) : null}
+                        {memo.code !== "" ? (
+                          <div className="shadow-sm">
+                            <CodeEditor
+                              value={memo?.code}
+                              language={memo?.codeType}
+                              disabled
+                              placeholder="Please enter some code."
+                              padding={15}
+                              style={{
+                                fontSize: 12,
+                                backgroundColor: "#f5f5f5",
+                                fontFamily:
+                                  "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+                              }}
+                            />
+                          </div>
+                        ) : null}
                         <div>
-                          <CodeEditor
-                            value={memo?.code}
-                            language="js"
-                            disabled
-                            placeholder="Please enter JS code."
-                            padding={15}
-                            style={{
-                              fontSize: 12,
-                              backgroundColor: "#f5f5f5",
-                              fontFamily:
-                                "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+                          <button
+                            className="p-2 flex gap-2 bg-red-500 text-white rounded shadow-sm"
+                            onClick={() => {
+                              deleteMemo(memo.id);
                             }}
-                          />
+                          >
+                            <AiOutlineDelete size={25} />
+                            <span className="uppercase text-center">
+                              Delete
+                            </span>
+                          </button>
                         </div>
-
-                        <button
-                          onClick={() => {
-                            deleteMemo(memo.id);
-                          }}
-                        >
-                          delete
-                        </button>
                       </div>
                     </>
                   );
                 })}
-              </>
+              </div>
             </div>
           )}
         </div>
---
+++ src/data/languages.ts
@@ -0,0 +1,62 @@
+export const languages = [
+  {
+    language: "Javascript",
+    extension: "js",
+  },
+  {
+    language: "Python",
+    extension: "py",
+  },
+  {
+    language: "Go",
+    extension: "go",
+  },
+  {
+    language: "Java",
+    extension: "java",
+  },
+  {
+    language: "Kotlin",
+    extension: "kt",
+  },
+  {
+    language: "PHP",
+    extension: "php",
+  },
+  {
+    language: "C#",
+    extension: "cs",
+  },
+  {
+    language: "Swift",
+    extension: "swift",
+  },
+  {
+    language: "R",
+    extension: "r",
+  },
+  {
+    language: "Ruby",
+    extension: "rb",
+  },
+  {
+    language: "C",
+    extension: "c",
+  },
+  {
+    language: "C++",
+    extension: "c",
+  },
+  {
+    language: "Matlab",
+    extension: "mlx",
+  },
+  {
+    language: "TypeScript",
+    extension: "ts",
+  },
+  {
+    language: "Scala",
+    extension: "sc",
+  },
+];
